ff3abc51d716cd22591587e66a2251ff4dfb7425
Amjesh <amjesh@Amjeshs-MacBook-Pro.local>
2023-02-14 16:44:03 +0530

    fix(webapp): commit base code

--- packages/webapp/src/pages/_app.tsx
+++ packages/webapp/src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import Metadata from "@/components/seo/metadata";
+import { SSRProvider } from "react-bootstrap";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Metadata />
-      <Component {...pageProps} />
+      <SSRProvider>
+        <Metadata />
+        <Component {...pageProps} />
+      </SSRProvider>
     </>
   );
 }
